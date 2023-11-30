5b4d28e437e28a403c22e7b7d92df62546479624
alien <148527832+itsalien024@users.noreply.github.com>
2023-11-30 12:52:48 +0300

    Rewriting JS from scratch // Made player and computer selection functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,103 +1,59 @@
-function computersMove() {
-  const num = 3;
-  let compMove;
-  let rng;
-  rng = Math.floor(Math.random() * num);
+let rock = document.querySelector('#rock');
+let paper = document.querySelector('#paper');
+let scissors = document.querySelector('#scissors');
+
+
+let playersMove = function() {
+  let playerSelection;
+  rock.onclick = function() {
+    playerSelection = 'rock';
+    return playerSelection;
+  }
+  paper.onclick = function() {
+    playerSelection = 'paper';
+    return playerSelection;
+  }
+  scissors.onclick = function() {
+    playerSelection = 'scissors';
+    return playerSelection;
+  }
+  return playerSelection;
+}
+
+let computersMove = function() {
+  let rng = Math.floor(Math.random() * 3);
+  let computerSelection;
   
   if (rng == 0) {
-    compMove = 'rock';
+    computerSelection = 'rock';
   } else if (rng == 1) {
-    compMove = 'paper';
+    computerSelection = 'paper';
   } else if (rng == 2) {
-    compMove = 'scissors'
+    computerSelection = 'scissors';
   }
-  return compMove;
+  return computerSelection;
 }
 
-function playersMove(move) {
-  let result;
-  move = prompt('Rock, Paper or Scissors? ->');
-  if (move.toLowerCase() == "rock") {
-    result = 'rock';
-  } else if (move.toLowerCase() == "paper") {
-    result = 'paper'
-  } else if (move.toLowerCase() == "scissors") {
-    result = 'scissors';
-  } else {
-    console.log('Please choose a valid move.')
-  }
-  return result; 
-}
 
-let computerScore = 0;
-let playerScore = 0; 
-let cMove = computersMove();
-let pMove = playersMove();
 
-function game(cMove, pMove) {
-  switch (true) {
-    case (cMove == 'rock' && pMove == 'rock'):
-      console.log('Its a tie!');
-      console.log('You chose Rock, The computer chose Rock');
-      console.log('Player Score: ' + playerScore, 'Computer Score: ' + computerScore,);
+let playerWinOptions = function() {
+  let winMessage;
+  switch(true) {
+    case playersMove() == 'rock' && computersMove() == 'scissors':
+      winMessage = 'You Chose Rock, Computer Chose Scissors. You Win!'
       break;
 
-    case (cMove == 'rock' && pMove == 'paper'):
-      playerScore ++;
-      console.log('Player Wins!');
-      console.log('You chose Paper, The computer chose Rock');
-      console.log('Player Score: ' + playerScore, 'Computer Score: ' + computerScore,);
+    case playersMove() == 'paper' && computersMove() == 'rock':
+      winMessage = 'You Chose Paper, Computer Chose Rock. You Win!'
       break;
 
-    case (cMove == 'rock' && pMove == 'scissors'):
-      computerScore ++;
-      console.log('Computer Wins!');
-      console.log('You chose Scissors, The computer chose Rock');
-      console.log('Player Score: ' + playerScore, 'Computer Score: ' + computerScore,);
-      break;
-    
-    case (cMove == 'paper' && pMove == 'rock'):
-      computerScore ++;
-      console.log('Computer Wins!');
-      console.log('You chose Rock, The computer chose Paper');
-      console.log('Player Score: ' + playerScore, 'Computer Score: ' + computerScore,);
-      break;
-
-    case (cMove == 'paper' && pMove == 'paper'):
-      console.log('Its a tie!');
-      console.log('You chose Paper, The computer chose Paper');
-      console.log('Player Score: ' + playerScore, 'Computer Score: ' + computerScore,);
-      break;
-        
-    case (cMove == 'paper' && pMove == 'scissors'):
-      playerScore ++;
-      console.log('Player Wins');
-      console.log('You chose Scissors, The computer chose Paper');
-      console.log('Player Score: ' + playerScore, 'Computer Score: ' + computerScore,);
-      break;
-
-    case (cMove == 'scissors' && pMove == 'rock'):
-      playerScore ++;
-      console.log('Player Wins!');
-      console.log('You chose Rock, The computer chose Scissors');
-      console.log('Player Score: ' + playerScore, 'Computer Score: ' + computerScore,);
-      break;
-
-    case (cMove == 'scissors' && pMove == 'paper'):
-      computerScore ++;
-      console.log('Computer Wins!');
-      console.log('You chose Paper, The computer chose Scissors');
-      console.log('Player Score: ' + playerScore, 'Computer Score: ' + computerScore,);
-      break;
-        
-    case (cMove == 'scissors' && pMove == 'scissors'):
-      console.log('Its a tie!');
-      console.log('You chose Scissors, The computer chose Scissors');
-      console.log('Player Score: ' + playerScore, 'Computer Score: ' + computerScore,);
+    case playersMove() == 'scissors' && computersMove() == 'paper':
+      winMessage = 'You Chose Scissors, Computer Chose Paper. You Win!'
       break;
   }
+  return winMessage;
 }
 
-console.log(game(cMove, pMove));
+
 
 
