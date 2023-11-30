a1ff07d1bd6bf31eac32709847f95bc2e57315de
alien <148527832+itsalien024@users.noreply.github.com>
2023-12-01 01:13:11 +0300

    Fully functioning code for RPS // made rounds run each time button is clicked

--- scripts/functionality.js
+++ scripts/functionality.js
@@ -1,3 +1,131 @@
+let rock = document.querySelector('#rock');
+let paper = document.querySelector('#paper');
+let scissors = document.querySelector('#scissors');
+let message = document.querySelector('#winnerMessage');
+let playerScore = document.querySelector('#playerScoreNumber');
+let computerScore = document.querySelector('#computerScoreNumber');
+let winner;
+let playersMove;
+
+playerScore = 0;
+computerScore = 0;
+
+let computersMove = function() {
+  let rng = Math.floor(Math.random() * 3);
+  let computerSelection;
+  
+  if (rng == 0) {
+    computerSelection = 'rock';
+  } else if (rng == 1) {
+    computerSelection = 'paper';
+  } else if (rng == 2) {
+    computerSelection = 'scissors';
+  }
+  return computerSelection;
+}
+
+let reset = function () {
+  playerScore = 0;
+  computerScore = 0;
+}
 
 
+rock.onclick = function () {
+  let cMove = computersMove();
+  playersMove = 'rock';
+
+  switch(true) {
+    case playersMove == 'rock' && cMove == 'rock':
+      message.innerHTML = ('Its A Tie!')
+      break;
+
+    case playersMove == 'rock' && cMove == 'paper':
+      computerScore++
+      message.innerHTML = ('Computer Wins!')
+      computerScoreNumber.innerHTML = (computerScore);
+      break;
+
+    case playersMove == 'rock' && cMove == 'scissors':
+      playerScore++
+      message.innerHTML = ('Player Wins!')
+      playerScoreNumber.innerHTML = (playerScore);
+      break;
+  }
+
+  if (playerScore == 10) {
+    message.innerHTML = ('Player Wins The Game!')
+    reset();
+  }else if (computerScore == 10) {
+    message.innerHTML = ('Comouter Wins The Game!')
+    reset();
+  }
+
+  return;
+}
+
+paper.onclick = function () {
+  let cMove = computersMove();
+  playersMove = 'paper';
+
+  switch(true) {
+    case playersMove == 'paper' && cMove == 'rock':
+      playerScore++
+      message.innerHTML = ('Player Wins!')
+      playerScoreNumber.innerHTML = (playerScore);
+      break;
+
+    case playersMove == 'paper' && cMove == 'paper':
+      message.innerHTML = ('Its A Tie!')
+      break;
+
+    case playersMove == 'paper' && cMove == 'scissors':
+      computerScore++
+      message.innerHTML = ('Computer Wins!')
+      computerScoreNumber.innerHTML = (computerScore);
+      break;
+  }
+
+    if (playerScore == 10) {
+      message.innerHTML = ('Player Wins The Game!')
+      reset();
+    }else if (computerScore == 10) {
+      message.innerHTML = ('Comouter Wins The Game!')
+      reset();
+    }
+
+  return ;
+}
+
+scissors.onclick = function () {
+  let cMove = computersMove();
+  playersMove = 'scissors';
+
+  switch(true) {
+    case playersMove == 'scissors' && cMove == 'rock':
+      computerScore++
+      message.innerHTML = ('Computer Wins!')
+      computerScoreNumber.innerHTML = (computerScore);
+      break;
+
+    case playersMove == 'scissors' && cMove == 'paper':
+      playerScore++
+      message.innerHTML = ('Player Wins!')
+      playerScoreNumber.innerHTML = (playerScore);
+      break;
+
+    case playersMove == 'scissors' && cMove == 'scissors':
+      message.innerHTML = ('Its A Tie!')
+      break;
+  }
+
+  if (playerScore == 10) {
+    message.innerHTML = ('Player Wins The Game!')
+    reset();
+  }else if (computerScore == 10) {
+    message.innerHTML = ('Comouter Wins The Game!')
+    reset();
+  }
+
+  return ;
+}
 
--- scripts/script.js
+++ scripts/script.js
@@ -1,59 +1,119 @@
-let rock = document.querySelector('#rock');
-let paper = document.querySelector('#paper');
-let scissors = document.querySelector('#scissors');
+// let rock = document.querySelector('#rock');
+// let paper = document.querySelector('#paper');
+// let scissors = document.querySelector('#scissors');
 
 
-let playersMove = function() {
-  let playerSelection;
-  rock.onclick = function() {
-    playerSelection = 'rock';
-    return playerSelection;
-  }
-  paper.onclick = function() {
-    playerSelection = 'paper';
-    return playerSelection;
-  }
-  scissors.onclick = function() {
-    playerSelection = 'scissors';
-    return playerSelection;
-  }
-  return playerSelection;
-}
+// let playersMove = function() {
+//   let playerSelection;
+//   rock.onclick = function() {
+//     playerSelection = 'rock';
+//     return playerSelection;
+//   }
+//   paper.onclick = function() {
+//     playerSelection = 'paper';
+//     return playerSelection;
+//   }
+//   scissors.onclick = function() {
+//     playerSelection = 'scissors';
+//     return playerSelection;
+//   }
+//   return playerSelection;
+// }
 
-let computersMove = function() {
-  let rng = Math.floor(Math.random() * 3);
-  let computerSelection;
+// let computersMove = function() {
+//   let rng = Math.floor(Math.random() * 3);
+//   let computerSelection;
   
-  if (rng == 0) {
-    computerSelection = 'rock';
-  } else if (rng == 1) {
-    computerSelection = 'paper';
-  } else if (rng == 2) {
-    computerSelection = 'scissors';
-  }
-  return computerSelection;
-}
+//   if (rng == 0) {
+//     computerSelection = 'rock';
+//   } else if (rng == 1) {
+//     computerSelection = 'paper';
+//   } else if (rng == 2) {
+//     computerSelection = 'scissors';
+//   }
+//   return computerSelection;
+// }
 
 
+// let playerWinOptions = function() {
+//   let winMessage;
+  // switch(true) {
+  //   case playersMove() == 'rock' && computersMove() == 'scissors':
+  //     winMessage = 'pWin'
+  //     break;
 
-let playerWinOptions = function() {
-  let winMessage;
-  switch(true) {
-    case playersMove() == 'rock' && computersMove() == 'scissors':
-      winMessage = 'You Chose Rock, Computer Chose Scissors. You Win!'
-      break;
+  //   case playersMove() == 'paper' && computersMove() == 'rock':
+  //     winMessage = 'pWin'
+  //     break;
 
-    case playersMove() == 'paper' && computersMove() == 'rock':
-      winMessage = 'You Chose Paper, Computer Chose Rock. You Win!'
-      break;
+  //   case playersMove() == 'scissors' && computersMove() == 'paper':
+  //     winMessage = 'pWin'
+  //     break;
+  // }
+  // return winMessage;
+// }
 
-    case playersMove() == 'scissors' && computersMove() == 'paper':
-      winMessage = 'You Chose Scissors, Computer Chose Paper. You Win!'
-      break;
-  }
-  return winMessage;
-}
+// let computerWinOptions = function() {
+//   let winMessage;
+//   switch(true) {
+//     case computersMove() == 'rock' && playersMove() == 'scissors':
+//       winMessage = 'cWin'
+//       break;
 
+//     case computersMove() == 'paper' && playersMove() == 'rock':
+//       winMessage = 'cWin'
+//       break;
 
+//     case computersMove() == 'scissors' && playersMove() == 'paper':
+//       winMessage = 'cWin'
+//       break;
+//   }
+//   return winMessage;
+// }
 
+// let tieOptions = function() {
+//   let tieMessage;
+//   switch(true) {
+//     case computersMove() == 'rock' && playersMove() == 'rock':
+//       tieMessage = 'tie'
+//       break;
+
+//     case computersMove() == 'paper' && playersMove() == 'paper':
+//       tieMessage = 'tie'
+//       break;
+
+//     case computersMove() == 'scissors' && playersMove() == 'scissors':
+//       tieMessage = 'tie'
+//       break;
+//   }
+//   return tieMessage;
+// }
+
+// function gameRound(playerWinOptions, computerWinOptions, tieOptions) {
+//   let winner;
+//   if (playerWinOptions() == 'pWin') {
+//     winner = 'player';
+//   }else if (computerWinOptions() == 'cWin') {
+//     winner = 'computer';
+//   }else if (tieOptions() == 'tie') {
+//     winner = 'tie';
+//   }
+//   return winner;
+// }
+
+// let getPlayerScore = function() {
+//   let playerScore = 0;
+//   if (gameRound() == 'player') {
+//     playerScore ++;
+//   }
+//   return playerScore;
+// }
+
+// let getComputerScore = function() {
+//   let computerScore = 0;
+//   if (gameRound() == 'computer') {
+//     computerScore ++;
+//   }
+//   return computerScore;
+// }
 
